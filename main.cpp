30a28799950f35b1fda6d29eb0cb5978c2e45e94
yorielyssantos <yorielys.delossantos@upr.edu>
2025-11-16 13:12:28 -0400

    web

--- main.cpp
+++ main.cpp
@@ -8,4 +8,18 @@ int main() {
   G.addEdge(4, 2);
 
   G.print();
+  cout << endl;
+
+  cout << "Número total de aristas: " << G.numEdges() << "\n\n";
+
+  cout << "GradoEntrada de 9: " << G.inDegree(9) << "\n";
+  cout << "GradoEntrada de 2: " << G.inDegree(2) << "\n";
+  cout << "GradoEntrada de 0: " << G.inDegree(0) << "\n\n";
+
+  for (int fila = 0; fila < 10; ++fila) {
+        if (G.isInfluencer(fila)) {
+            cout << "El nodo \'" << fila << "\' es un influencer.\n";
+        }
+    }
+    
 }
